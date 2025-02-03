915547830d4c68fe0c8c23c06f5571a399968358
tusar khan <114900895+MrTusarRX@users.noreply.github.com>
2025-02-03 12:26:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
-// index.php
 
-session_start(); // Start the session
 
-// Check if the user is logged in
+session_start(); 
+
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    // If not logged in, redirect to login page
     header("Location: index.html");
     exit;
 }
-
-// Your content for the logged-in user
 echo "Welcome, " . $_SESSION['username'] . "! You are logged in.";
 ?>
